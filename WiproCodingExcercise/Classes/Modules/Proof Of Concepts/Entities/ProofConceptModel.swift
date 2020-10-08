3d82fe4b1057063c85fcf03d01b36951876f9f66
Prince Sojitra <princehsojitra@gmail.com>
2020-10-08 15:02:04 +0530

    Model created for proof of concept feed data

---
+++ WiproCodingExcercise/Classes/Modules/Proof Of Concepts/Entities/ProofConceptModel.swift
@@ -0,0 +1,25 @@
+
+//
+//	ProofConceptModel.swift
+//	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
+
+import Foundation
+
+struct ProofConceptModel : Codable {
+
+	let rows : [ProofConceptRow]?
+	let title : String?
+
+
+	enum CodingKeys: String, CodingKey {
+		case rows = "rows"
+		case title = "title"
+	}
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		rows = try values.decodeIfPresent([ProofConceptRow].self, forKey: .rows)
+		title = try values.decodeIfPresent(String.self, forKey: .title)
+	}
+
+
+}
---
+++ WiproCodingExcercise/Classes/Modules/Proof Of Concepts/Entities/ProofConceptRow.swift
@@ -0,0 +1,28 @@
+//
+//	ProofConceptRow.swift
+//	Model file generated using JSONExport: https://github.com/Ahmed-Ali/JSONExport
+
+import Foundation
+
+struct ProofConceptRow : Codable {
+
+	let descriptionField : String?
+	let imageHref : String?
+	let title : String?
+
+
+	enum CodingKeys: String, CodingKey {
+		case descriptionField = "description"
+		case imageHref = "imageHref"
+		case title = "title"
+	}
+    
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		descriptionField = try values.decodeIfPresent(String.self, forKey: .descriptionField)
+		imageHref = try values.decodeIfPresent(String.self, forKey: .imageHref)
+		title = try values.decodeIfPresent(String.self, forKey: .title)
+	}
+
+
+}
