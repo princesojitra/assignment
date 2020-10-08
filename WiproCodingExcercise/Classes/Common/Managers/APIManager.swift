6bdad4b219b126876b114e379c2e045609062353
Prince Sojitra <princehsojitra@gmail.com>
2020-10-08 15:34:53 +0530

    API manager classes integrated to fatch feed data from server side

---
+++ WiproCodingExcercise/Classes/Common/Managers/APIManager.swift
@@ -0,0 +1,126 @@
+//
+//  APIManager.swift
+//  WiproCodingExcercise
+//
+//  Created by Prince Sojitra on 07/10/20.
+//
+
+import UIKit
+
+
+typealias Parameters = [String:String]
+typealias ResponseBody = Any
+typealias ReesultComplitionBlock = (Result<Decodable?,APIError>) -> Void
+typealias HTTPHeaders = [String: String]
+
+//Error types
+enum APIError: Error {
+    case requestFailed
+    case failedToCreateURL
+    case invalidData
+    case jsonParsingFailure
+    case internetConecctionNotAvailable
+    var localizedDescription: String {
+        switch self {
+        case .requestFailed: return "Request Failed"
+        case .invalidData: return "Invalid Data"
+        case .failedToCreateURL: return "Faild to create URL"
+        case .jsonParsingFailure: return "JSON Parsing Failure"
+        case .internetConecctionNotAvailable : return "Internet connection appears to be offline"
+        }
+    }
+}
+
+//Service method types
+enum APIMethodType : String{
+    case get     = "GET"
+    case head    = "HEAD"
+    case post    = "POST"
+    case put     = "PUT"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+    case trace   = "TRACE"
+}
+
+//Server types
+enum APIServerType {
+    case Development
+    case Staging
+    case Production
+    var baseUrl:String {
+        switch self {
+        case .Development:
+            return "https://dl.dropboxusercontent.com/"
+        case .Staging:
+            return "https://dl.dropboxusercontent.com/"
+        case .Production:
+            return "https://dl.dropboxusercontent.com/"
+        }
+    }
+}
+
+
+
+//Response Type
+enum Result<T, U> where U: Error  {
+    case success(T)
+    case failure(U)
+}
+
+//Endpoints requirements
+protocol APIEndPointProtocol {
+    var path: String { get set }
+    var method: APIMethodType  { get set }
+    var parameter: Parameters?  { get set }
+    var resultCompletion: ReesultComplitionBlock?  { get set }
+}
+
+
+//Endpoints details
+class APIEndpoint :APIEndPointProtocol {
+    var path: String = ""
+    var method: APIMethodType
+    var parameter: Parameters?
+    var headers: HTTPHeaders?
+    var resultCompletion: ReesultComplitionBlock?
+    var showLoader: Bool = true
+    
+    init(path: String, method:APIMethodType  = .get, parameter: Parameters? = nil, showLoader: Bool = true ,headers:HTTPHeaders = APIManager.httpsHeaders(withtoken: nil),  completion: @escaping ReesultComplitionBlock) {
+        self.path = path
+        self.method = method
+        self.parameter = parameter
+        self.resultCompletion = completion
+        self.showLoader = showLoader
+        self.headers = headers
+    }
+}
+
+class APIManager: NSObject {
+    
+    static let shared = { APIManager() }()
+    
+    //Get deployement server type
+    static var server :APIServerType {
+        return .Development
+    }
+  
+    
+    //Get baseurl based on server type
+    lazy var baseURL: String = {
+        return APIManager.server.baseUrl
+    }()
+    
+    
+    //Set headers
+    class func httpsHeaders(withtoken token: String?) -> HTTPHeaders {
+        var httpHeaders = ["content-type": "application/json","accept": "application/json",]
+        guard let authToken = token else {
+            return httpHeaders
+        }
+        httpHeaders["Authorization"] = "Bearer \(authToken)"
+        
+        return httpHeaders
+    }
+}
+
+
---
+++ WiproCodingExcercise/Classes/Common/Managers/NetworkManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkManager.swift
+//  WiproCodingExcercise
+//
+//  Created by Prince Sojitra on 07/10/20.
+//
+
+import UIKit
+import Reachability
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    let reachability = try! Reachability()
+    
+    private init(){
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
+        do{
+            try reachability.startNotifier()
+        }catch{
+            print("could not start reachability notifier")
+        }
+    }
+    
+    @objc func reachabilityChanged(note: Notification) {
+        
+        let reachability = note.object as! Reachability
+        
+        switch reachability.connection {
+        case .wifi:
+            print("Reachable via WiFi")
+        case .cellular:
+            print("Reachable via Cellular")
+        case .unavailable:
+            print("Network not reachable")
+        case .none:
+            print("Network not available")
+        }
+    }
+    
+    func isReachableNetwork() -> Bool {
+        switch reachability.connection {
+        case .wifi, .cellular:
+            return true
+        case .unavailable, .none:
+            return false
+        }
+    }
+}
+
+
