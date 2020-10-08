9b6ccc0b2cbe134bafd0e7f014df96cb2edddff8
Prince Sojitra <princehsojitra@gmail.com>
2020-10-08 14:44:28 +0530

    Folder structured added

---
+++ WiproCodingExcercise/Classes/Common/Constants/Constants.swift
@@ -0,0 +1,56 @@
+//
+//  Constants.swift
+//  WiproCodingExcercise
+//
+//  Created by Prince Sojitra on 07/10/20.
+//
+
+
+import UIKit
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let appName = Bundle.main.infoDictionary!["CFBundleName"] as! String
+let appVersion = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
+let devieType = "iOS"
+let apiKey = "2iodh4vg0eortkl"
+let deviceId = UIDevice.current.identifierForVendor!.uuidString
+let systemVersion = UIDevice.current.systemVersion
+let USERDEFAULTS = UserDefaults.standard
+var indicator:UIActivityIndicatorView?
+
+struct TblCellIdentifier {
+    static let ProfOfConceptFeedList = "ProfOfConceptFeedListTblCell"
+}
+
+func activityIndicator() {
+    if #available(iOS 13.0, *) {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        
+        if(indicator == nil) {
+            indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+            indicator?.style = UIActivityIndicatorView.Style.medium
+            indicator?.center =  CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+            indicator?.backgroundColor = .white
+            indicator?.hidesWhenStopped = true
+            keyWindow?.addSubview(indicator!)
+        }
+    }
+    else{
+        // Fallback on earlier versions
+        let keyWindow = UIApplication.shared.keyWindow
+        if(indicator == nil) {
+            indicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+            indicator?.style = UIActivityIndicatorView.Style.gray
+            indicator?.center =  CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+            indicator?.backgroundColor = .white
+            indicator?.hidesWhenStopped = true
+            keyWindow?.addSubview(indicator!)
+        }
+    }
+}
+
---
+++ WiproCodingExcercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WiproCodingExcercise
+//
+//  Created by Prince Sojitra on 07/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
